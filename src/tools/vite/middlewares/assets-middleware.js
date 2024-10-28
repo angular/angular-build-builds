fec382dc78ffcb8015fbae40e017fb5a9b174ba7
Angular Builds <circleci@angular.dev>
2024-10-28 18:20:43 +0000

    7229092 refactor(@angular/build): use ETags for dev-server resource requests

--- src/tools/vite/middlewares/assets-middleware.js
+++ src/tools/vite/middlewares/assets-middleware.js
@@ -100,11 +100,19 @@ function createAngularAssetsMiddleware(server, assets, outputFiles, usedComponen
                         }
                     }
                 }
+                // Avoid resending the content if it has not changed since last request
+                const etag = `W/"${outputFile.contents.byteLength}-${outputFile.hash}"`;
+                if (req.headers['if-none-match'] === etag) {
+                    res.statusCode = 304;
+                    res.end();
+                    return;
+                }
                 const mimeType = (0, mrmime_1.lookup)(extension);
                 if (mimeType) {
                     res.setHeader('Content-Type', mimeType);
                 }
                 res.setHeader('Cache-Control', 'no-cache');
+                res.setHeader('ETag', etag);
                 res.end(data);
                 return;
             }
