04f42fab71acfb985e2b8d6dfcc746ee132653a7
Angular Builds <dev-infra@angular.dev>
2024-12-05 19:23:03 +0000

    f46d0b1 refactor(@angular/build): move `getDepOptimizationConfig` into utils file

--- src/tools/vite/utils.js
+++ src/tools/vite/utils.js
@@ -9,8 +9,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.pathnameWithoutBasePath = pathnameWithoutBasePath;
 exports.lookupMimeTypeFromRequest = lookupMimeTypeFromRequest;
+exports.getDepOptimizationConfig = getDepOptimizationConfig;
 const mrmime_1 = require("mrmime");
+const node_module_1 = require("node:module");
 const node_path_1 = require("node:path");
+const utils_1 = require("../esbuild/utils");
 function pathnameWithoutBasePath(url, basePath) {
     const parsedUrl = new URL(url, 'http://localhost');
     const pathname = decodeURIComponent(parsedUrl.pathname);
@@ -25,4 +28,58 @@ function lookupMimeTypeFromRequest(url) {
         return 'image/x-icon';
     }
     return extension && (0, mrmime_1.lookup)(extension);
+}
+function getDepOptimizationConfig({ disabled, exclude, include, target, zoneless, prebundleTransformer, ssr, loader, thirdPartySourcemaps, }) {
+    const plugins = [
+        {
+            name: 'angular-browser-node-built-in',
+            setup(build) {
+                // This namespace is configured by vite.
+                // @see: https://github.com/vitejs/vite/blob/a1dd396da856401a12c921d0cd2c4e97cb63f1b5/packages/vite/src/node/optimizer/esbuildDepPlugin.ts#L109
+                build.onLoad({ filter: /.*/, namespace: 'browser-external' }, (args) => {
+                    if (!(0, node_module_1.isBuiltin)(args.path)) {
+                        return;
+                    }
+                    return {
+                        errors: [
+                            {
+                                text: `The package "${args.path}" wasn't found on the file system but is built into node.`,
+                            },
+                        ],
+                    };
+                });
+            },
+        },
+        {
+            name: `angular-vite-optimize-deps${ssr ? '-ssr' : ''}${thirdPartySourcemaps ? '-vendor-sourcemap' : ''}`,
+            setup(build) {
+                build.onLoad({ filter: /\.[cm]?js$/ }, async (args) => {
+                    return {
+                        contents: await prebundleTransformer.transformFile(args.path),
+                        loader: 'js',
+                    };
+                });
+            },
+        },
+    ];
+    return {
+        // Exclude any explicitly defined dependencies (currently build defined externals)
+        exclude,
+        // NB: to disable the deps optimizer, set optimizeDeps.noDiscovery to true and optimizeDeps.include as undefined.
+        // Include all implict dependencies from the external packages internal option
+        include: disabled ? undefined : include,
+        noDiscovery: disabled,
+        // Add an esbuild plugin to run the Angular linker on dependencies
+        esbuildOptions: {
+            // Set esbuild supported targets.
+            target,
+            supported: (0, utils_1.getFeatureSupport)(target, zoneless),
+            plugins,
+            loader,
+            define: {
+                'ngServerMode': `${ssr}`,
+            },
+            resolveExtensions: ['.mjs', '.js', '.cjs'],
+        },
+    };
 }
