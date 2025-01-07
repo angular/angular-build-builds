b0dd6326b7d7abb7266de33522e6f718ad5e6fa2
Angular Builds <dev-infra@angular.dev>
2025-01-07 14:27:14 +0000

    f3c6dfe refactor(@angular/build): remove no longer correct type

--- src/index.d.ts
+++ src/index.d.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-export { buildApplication, type ApplicationBuilderOptions, type ApplicationBuilderOutput, } from './builders/application';
+export { buildApplication, type ApplicationBuilderOptions } from './builders/application';
 export type { ApplicationBuilderExtensions } from './builders/application/options';
 export { type BuildOutputFile, BuildOutputFileType } from './tools/esbuild/bundler-context';
 export type { BuildOutputAsset } from './tools/esbuild/bundler-execution-result';
