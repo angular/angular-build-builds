ea9113bdb1b91ff012821c799f3801f9aade95d2
Angular Builds <dev-infra@angular.dev>
2025-01-09 16:13:21 +0000

    ba51e72 fix(@angular/build): use component updates for component style HMR

--- src/builders/dev-server/vite-server.js
+++ src/builders/dev-server/vite-server.js
@@ -103,7 +103,7 @@ async function* serveWithVite(serverOptions, builderName, builderAction, context
         // https://nodejs.org/api/process.html#processsetsourcemapsenabledval
         process.setSourceMapsEnabled(true);
     }
-    // Enable to support link-based component style hot reloading (`NG_HMR_CSTYLES=0` can be used to disable selectively)
+    // Enable to support link-based component style hot reloading (`NG_HMR_CSTYLES=1` can be used to enable)
     browserOptions.externalRuntimeStyles =
         serverOptions.liveReload && serverOptions.hmr && environment_options_1.useComponentStyleHmr;
     // Enable to support component template hot replacement (`NG_HMR_TEMPLATE=0` can be used to disable selectively)
