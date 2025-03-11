69db2ba619ba7ad9b0915c4401c66941de01cb50
Angular Builds <dev-infra@angular.dev>
2025-03-11 14:32:23 +0000

    ee8466d fix(@angular/ssr): prevent stream draining if `write` does not return a boolean

--- src/tools/vite/middlewares/chrome-devtools-middleware.js
+++ src/tools/vite/middlewares/chrome-devtools-middleware.js
@@ -6,8 +6,12 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createChromeDevtoolsMiddleware = createChromeDevtoolsMiddleware;
+const node_assert_1 = __importDefault(require("node:assert"));
 const node_crypto_1 = require("node:crypto");
 const node_fs_1 = require("node:fs");
 const node_path_1 = require("node:path");
@@ -20,23 +24,27 @@ function createChromeDevtoolsMiddleware(cacheDir, projectRoot) {
             next();
             return;
         }
-        // We store the UUID and re-use it to ensure Chrome does not repeatedly ask for permissions when restarting the dev server.
-        try {
-            devtoolsConfig ??= (0, node_fs_1.readFileSync)(devtoolsConfigPath, 'utf-8');
-        }
-        catch {
-            const devtoolsConfigJson = {
-                workspace: {
-                    root: projectRoot,
-                    uuid: (0, node_crypto_1.randomUUID)(),
-                },
-            };
-            devtoolsConfig = JSON.stringify(devtoolsConfigJson, undefined, 2);
+        if (!devtoolsConfig) {
+            // We store the UUID and re-use it to ensure Chrome does not repeatedly ask for permissions when restarting the dev server.
             try {
-                (0, node_fs_1.mkdirSync)(cacheDir, { recursive: true });
-                (0, node_fs_1.writeFileSync)(devtoolsConfigPath, devtoolsConfig);
+                const devtoolsConfig = (0, node_fs_1.readFileSync)(devtoolsConfigPath, 'utf-8');
+                const devtoolsConfigJson = JSON.parse(devtoolsConfig);
+                node_assert_1.default.equal(projectRoot, devtoolsConfigJson?.workspace.root);
             }
-            catch { }
+            catch {
+                const devtoolsConfigJson = {
+                    workspace: {
+                        root: projectRoot,
+                        uuid: (0, node_crypto_1.randomUUID)(),
+                    },
+                };
+                devtoolsConfig = JSON.stringify(devtoolsConfigJson, undefined, 2);
+                try {
+                    (0, node_fs_1.mkdirSync)(cacheDir, { recursive: true });
+                    (0, node_fs_1.writeFileSync)(devtoolsConfigPath, devtoolsConfig);
+                }
+                catch { }
+            }
         }
         res.setHeader('Content-Type', 'application/json');
         res.end(devtoolsConfig);
