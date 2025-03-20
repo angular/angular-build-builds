6a11fbd39dbdca02562f12459c3a866ce4254e69
Angular Builds <dev-infra@angular.dev>
2025-03-20 13:17:56 +0000

    d89c2cb refactor: use `platformBrowser` instead of `platformBrowserTesting`

--- src/builders/karma/polyfills/init_test_bed.js
+++ src/builders/karma/polyfills/init_test_bed.js
@@ -7,13 +7,12 @@
  */
 
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing';
+import { platformBrowser } from '@angular/platform-browser';
+import { BrowserTestingModule } from '@angular/platform-browser/testing';
 
+// TODO(alanagius): replace with `platformBrowserTesting` once https://github.com/angular/angular/pull/60480 is released.
 // Initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowser(), {
   errorOnUnknownElements: true,
   errorOnUnknownProperties: true,
 });
