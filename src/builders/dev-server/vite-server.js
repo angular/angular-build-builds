79f149f925b853d58bbf4d07076bf73fe2df58d9
Angular Builds <dev-infra@angular.dev>
2025-04-15 21:10:08 +0000

    5078c8b refactor(@angular/build): use `ngHmrMode` define for Vite prebundling

--- src/builders/dev-server/vite-server.js
+++ src/builders/dev-server/vite-server.js
@@ -308,7 +308,10 @@ async function* serveWithVite(serverOptions, builderName, builderAction, context
                 });
             }
             // Setup server and start listening
-            const serverConfiguration = await setupServer(serverOptions, generatedFiles, assetFiles, browserOptions.preserveSymlinks, externalMetadata, ssrMode, prebundleTransformer, target, (0, internal_1.isZonelessApp)(polyfills), componentStyles, templateUpdates, browserOptions.loader, browserOptions.define, extensions?.middleware, transformers?.indexHtml, thirdPartySourcemaps);
+            const serverConfiguration = await setupServer(serverOptions, generatedFiles, assetFiles, browserOptions.preserveSymlinks, externalMetadata, ssrMode, prebundleTransformer, target, (0, internal_1.isZonelessApp)(polyfills), componentStyles, templateUpdates, browserOptions.loader, {
+                ...browserOptions.define,
+                'ngHmrMode': browserOptions.templateUpdates ? 'true' : 'false',
+            }, extensions?.middleware, transformers?.indexHtml, thirdPartySourcemaps);
             server = await createServer(serverConfiguration);
             await server.listen();
             // Setup builder context logging for browser clients
