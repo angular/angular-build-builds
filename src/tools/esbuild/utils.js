d7ed23aaa24732bff342b293e7f25db8b087d3b0
Angular Builds <dev-infra@angular.dev>
2026-02-24 17:30:46 +0000

    d0e9e81 build: update minimum supported Node.js versions

--- src/tools/esbuild/utils.js
+++ src/tools/esbuild/utils.js
@@ -318,7 +318,7 @@ function transformSupportedBrowsersToTargets(supportedBrowsers) {
     }
     return transformed;
 }
-const SUPPORTED_NODE_VERSIONS = '^20.19.0 || ^22.12.0 || >=24.0.0';
+const SUPPORTED_NODE_VERSIONS = '^22.22.0 || >=24.13.1';
 /**
  * Transform supported Node.js versions to esbuild target.
  * @see https://esbuild.github.io/api/#target
