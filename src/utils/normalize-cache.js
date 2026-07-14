f1423051e74284ef0c0eda238401b514d562bb3a
Angular Builds <dev-infra@angular.dev>
2026-07-14 23:17:53 +0000

    b345bb5 fix(@schematics/angular): remove unused spec tsconfig outDir

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -11,7 +11,7 @@ exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_fs_1 = require("node:fs");
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.0-next.3+sha-b0b3a47';
+const VERSION = '22.1.0-next.3+sha-b345bb5';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
