d358fed28f3dca97f69237497ebe62fb16204c7c
Angular Builds <dev-infra@angular.dev>
2026-07-30 08:00:04 +0000

    db24220 fix(@angular/build): rewrite paths from sandboxed execroots when running under Bazel

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -11,7 +11,7 @@ exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_fs_1 = require("node:fs");
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.0+sha-4ee7d93';
+const VERSION = '22.1.0+sha-db24220';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
