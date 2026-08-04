a1b614bc982692aad67c3513ca8c26e478b64f09
Angular Builds <dev-infra@angular.dev>
2026-08-04 17:23:26 +0000

    4e54f3a ci: schedule ng-snapshot Renovate updates for early morning only

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -11,7 +11,7 @@ exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_fs_1 = require("node:fs");
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.2+sha-6f75b73';
+const VERSION = '22.1.2+sha-4e54f3a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
