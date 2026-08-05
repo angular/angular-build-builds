5121c70a8e4af8282c4013a9e52deda301a470cf
Angular Builds <dev-infra@angular.dev>
2026-08-05 18:28:40 +0000

    70461f6 perf(@angular/build): avoid encoding the inline source map before remapping

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -11,7 +11,7 @@ exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_fs_1 = require("node:fs");
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.3+sha-689b5b1';
+const VERSION = '22.1.3+sha-70461f6';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
