cee8ff34fa9248dfa4817c120205bd707a52b65f
Angular Builds <dev-infra@angular.dev>
2026-08-21 17:38:46 +0000

    a4c10f3 build: remove scorecard workflow

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -11,7 +11,7 @@ exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_fs_1 = require("node:fs");
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.5+sha-087d313';
+const VERSION = '22.1.5+sha-a4c10f3';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
