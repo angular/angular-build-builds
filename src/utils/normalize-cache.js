ba393a4845161749fa47e35a2c464feb116f790f
Angular Builds <dev-infra@angular.dev>
2026-09-02 21:10:22 +0000

    7e133c4 release: cut the v22.1.7 release

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -11,7 +11,7 @@ exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_fs_1 = require("node:fs");
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.6+sha-723b767';
+const VERSION = '22.1.7+sha-7e133c4';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
