d0849670f617a555313aa580da03d236a1e90c88
Angular Builds <dev-infra@angular.dev>
2026-09-10 14:24:56 +0000

    61cfc6d test(@angular/build): prevent flakiness in incremental-watch e2e test

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -11,7 +11,7 @@ exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_fs_1 = require("node:fs");
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.7+sha-ca322ca';
+const VERSION = '22.1.7+sha-61cfc6d';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
