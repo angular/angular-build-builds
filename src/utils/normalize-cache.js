f771e0c03e6336510463779838dc807593f91a9c
Angular Builds <dev-infra@angular.dev>
2026-09-22 17:35:20 +0000

    f52fb53 fix(@angular/cli): decouple official Node.js support check from execution gate

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -11,7 +11,7 @@ exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_fs_1 = require("node:fs");
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.3.0-next.0+sha-5e05373';
+const VERSION = '22.3.0-next.0+sha-f52fb53';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
